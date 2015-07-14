f9b010f49a9c6459cbe77d88da7b35dbe4652e0f
Yury Kotov <bairkan@yandex.ru>
2015-07-14 14:24:50 +0300

    Changed app url

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject guestbook "0.1.0-SNAPSHOT"
   :min-lein-version "2.0.0"
   :description "Guestbook study application"
-  :url "https://classique-croissant-5587.herokuapp.com"
+  :url "https://clojure-studies-guestbook.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
                  [hiccup "1.0.5"]
