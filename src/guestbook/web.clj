a53c03b4c288b2b21bc94281d40b8e6e91182ba8
Yury Kotov <bairkan@yandex.ru>
2015-07-15 12:29:31 +0300

    Try to use use sqlite in production

--- src/guestbook/web.clj
+++ src/guestbook/web.clj
@@ -1,16 +1,17 @@
 (ns guestbook.web
   (:use [ring.adapter.jetty]
-        [ring.middleware.resource]))
+        [ring.middleware.resource]
+        [guestbook.handler]))
 
 (defn get-handler []
   ;; #'app expands to (var app) so that when we reload our code,
   ;; the server is forced to re-resolve the symbol in the var
   ;; rather than having its own copy. When the root binding
   ;; changes, the server picks it up without having to restart.
-  (-> #'guestbook.handler/app
+  (-> #'app
     (wrap-resource "public")
     (wrap-resource "/META-INF/resources")))
 
 (defn -main []
-  (guestbook.handler/init)
+  (init)
   (run-jetty (get-handler) {:port (Integer/parseInt (or (System/getenv "PORT") "8080"))}))
