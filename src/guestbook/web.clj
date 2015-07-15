1d0e49d1cbf62ea4b61de0ba8888b29a3f40d926
Yury Kotov <bairkan@yandex.ru>
2015-07-15 12:08:25 +0300

    Try to use uberjar in production

--- src/guestbook/web.clj
+++ src/guestbook/web.clj
@@ -1,16 +1,17 @@
 (ns guestbook.web
   (:use [ring.adapter.jetty]
-        [ring.middleware.resource]))
+        [guestbook.handler :refer [app]))
 
-(defn handler [request]
-    {:status 404
-     :headers {"Content-Type" "text/html"}
-     :body (str "Cannot find " (:uri request))})
-
-(def app
-  (-> guestbook.handler/app
-    (wrap-resource "public")
-    (wrap-resource "/META-INF/resources")))
+(defn get-handler []
+  ;; #'app expands to (var app) so that when we reload our code,
+  ;; the server is forced to re-resolve the symbol in the var
+  ;; rather than having its own copy. When the root binding
+  ;; changes, the server picks it up without having to restart.
+  (-> #'app
+    ; Makes static assets in $PROJECT_DIR/resources/public/ available.
+    (wrap-file "resources")
+    ; Content-Type, Content-Length, and Last Modified headers for files in body
+    (wrap-file-info)))
 
 (defn -main []
-  (run-jetty app {:port (Integer/parseInt (or (System/getenv "PORT") "8080"))}))
+  (run-jetty (get-handler) {:port (Integer/parseInt (or (System/getenv "PORT") "8080"))}))
